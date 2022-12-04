e780dfed7e770d9faa7b2c33545eff74547a711d
iamMAHAM <abdulkabore@gmail.com>
2022-12-04 09:13:41 +0000

    update

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -11,12 +11,12 @@ export default NextAuth({
   secret:process.env.JWTPASSWORD,
   callbacks: {
     async jwt({ token, user }: any) {
-      console.log(user)
       if (user?._id) token.id = user._id;
       if (user?.isAdmin) token.isAdmin = user.isAdmin;
       return token;
     },
     async session({ session, token }) {
+      console.log(session)
       if (token?._id) token.id = token._id;
       if (token?.isAdmin) token.isAdmin = token.isAdmin;
       return session;
